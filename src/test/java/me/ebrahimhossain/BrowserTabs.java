a141b1849bb157c7bf8dd55493b4953eb11bc040
ebrahimhossaincse <mdebrahimhossain.me@gmail.com>
2025-03-08 05:51:34 -0500

    pom

--- src/test/java/me/ebrahimhossain/BrowserTabs.java
+++ src/test/java/me/ebrahimhossain/BrowserTabs.java
@@ -1,4 +1,60 @@
 package me.ebrahimhossain;
 
+import com.microsoft.playwright.*;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
 public class BrowserTabs {
+    Playwright playwright;
+    BrowserType browserType;
+    Browser browser;
+    BrowserContext browserContext;
+    Page page;
+
+    @BeforeSuite
+    public void start(){
+        playwright = Playwright.create();
+        browserType = playwright.chromium();
+        browser = browserType.launch(new BrowserType.LaunchOptions().setHeadless(false));
+        browserContext = browser.newContext();
+        page = browser.newPage();
+        System.out.println("Browser version: " + browser.version());
+    }
+
+   // @Test
+    public void openUrl() throws InterruptedException {
+        Page firstTab = browserContext.newPage();
+        firstTab.navigate("https://testing-and-learning-hub.vercel.app/Selenium/pages/browser_windows.html");
+        Thread.sleep(3000);
+
+        Page secondTab = browserContext.newPage();
+        secondTab.bringToFront();
+        secondTab.navigate("https://testing-and-learning-hub.vercel.app");
+        Thread.sleep(4000);
+
+        firstTab.bringToFront();
+        Thread.sleep(4000);
+    }
+
+    @Test
+    public void windowHandle() throws InterruptedException {
+        page.navigate("https://testing-and-learning-hub.vercel.app/Selenium/pages/browser_windows.html");
+        Thread.sleep(3000);
+
+        Page secondWindow = browser.newContext().newPage();
+        secondWindow.bringToFront();
+        secondWindow.navigate("https://testing-and-learning-hub.vercel.app");
+        Thread.sleep(4000);
+
+        page.bringToFront();
+        Thread.sleep(4000);
+    }
+
+    @AfterSuite
+    public void stop(){
+        page.close();
+        browser.close();
+        playwright.close();
+    }
 }
--- src/test/java/me/ebrahimhossain/TableHandling.java
+++ src/test/java/me/ebrahimhossain/TableHandling.java
@@ -1,4 +1,79 @@
 package me.ebrahimhossain;
 
+import com.microsoft.playwright.*;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
 public class TableHandling {
+    Playwright playwright;
+    BrowserType browserType;
+    Browser browser;
+    BrowserContext browserContext;
+    Page page;
+
+    @BeforeSuite
+    public void start(){
+        playwright = Playwright.create();
+        browserType = playwright.chromium();
+        browser = browserType.launch(new BrowserType.LaunchOptions().setHeadless(false));
+        browserContext = browser.newContext();
+        page = browser.newPage();
+        System.out.println("Browser version: " + browser.version());
+    }
+
+    @Test
+    public void openUrl() throws InterruptedException {
+        Thread.sleep(2000);
+        page.navigate("https://testing-and-learning-hub.vercel.app/Selenium/pages/web_table.html");
+        Thread.sleep(5000);
+    }
+
+
+    public void fetchTableHeading() throws InterruptedException {
+        List<ElementHandle> headers = page.querySelectorAll("//table[@id='table1']/thead/tr/th");
+        System.out.println("Table headings: "+headers.size());
+
+        for (ElementHandle header : headers) {
+            String value = header.textContent();
+            System.out.println("Heading Value: "+value);
+        }
+    }
+
+    //@Test(dependsOnMethods = "openUrl")
+    public void fetchSpecificValue() throws InterruptedException {
+        List<ElementHandle> table_rows = page.querySelectorAll("//table[@id='table1']/tbody/tr");
+
+        for(int i=1; i<=table_rows.size(); i++){
+            ElementHandle email_locator = page.querySelector("//table[@id='table1']/tbody/tr["+i+"]/td[5]");
+            String email = email_locator.textContent();
+            System.out.println("Email: "+email);
+        }
+
+    }
+
+    @Test(dependsOnMethods = "openUrl")
+    public void fetchColValue() throws InterruptedException {
+        List<ElementHandle> table_rows = page.querySelectorAll("//table[@id='table1']/tbody/tr");
+
+        for(int i=1; i<=table_rows.size(); i++){
+            List<ElementHandle> table_cols = page.querySelectorAll("//table[@id='table1']/tbody/tr[1]/td");
+            for(int j=1; j<=table_cols.size(); j++){
+                ElementHandle col_locator = page.querySelector("//table[@id='table1']/tbody/tr["+i+"]/td["+j+"]");
+                String value = col_locator.textContent();
+                System.out.println(value);
+            }
+            System.out.println("===========================================================");
+        }
+
+    }
+
+    @AfterSuite
+    public void stop(){
+        page.close();
+        browser.close();
+        playwright.close();
+    }
 }
